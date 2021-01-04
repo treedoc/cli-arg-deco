fcc3fa45c5a400da920847a8dee9d84879c08565
Jianwu Chen <jianwu@gmail.com>
2021-01-04 07:34:34 -0800

    v0.0.2

--- src/core/LangUtil.ts
+++ src/core/LangUtil.ts
@@ -2,7 +2,7 @@ export type Constructor = (...args: any[]) => any;  // Add just to avoid JS Lint
 export function doIfNotNull<T>(obj: T | undefined, action: (obj: T) => void) { if (obj !== undefined && obj !== null)  action(obj); }
 export function doIf(condition: boolean, action: () => void) { if (condition) action(); }
 
-//TS_SPECIFIC
+// TS_SPECIFIC
 export function EnumValues(enumType: any) {
   return Object.keys(enumType).filter(key => !isNaN(Number(enumType[key])));
 }
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import CliArgDeco from './CliDeco';
+import CliSpec from './CliSpec';
+import CliParser from './CliParser';
+import Param from './Param';
+
+export {
+  CliArgDeco,
+  CliSpec,
+  CliParser,
+  Param,
+}
