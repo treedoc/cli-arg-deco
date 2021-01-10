4311f848bc411d1595359d40a1565b12da5ba83b
Jianwu Chen <jianwu@gmail.com>
2021-01-10 15:46:49 -0800

    v0.0.5

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import CliArgDeco from './CliDeco';
 import CliSpec from './CliSpec';
 import CliParser from './CliParser';
 import Param from './Param';
+import { EnumValues, EnumsValueOf } from './core/LangUtil';
 
 export {
   CliArgDeco,
   CliSpec,
   CliParser,
   Param,
+  EnumValues,
+  EnumsValueOf,
 }
